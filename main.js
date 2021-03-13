37e354cb33780af69d029b310802a5c36ce30538
beniskova <hana.rottova@gmail.com>
2021-03-13 10:51:05 +0100

    hotovo
    Co-Authored-By: Hana Pospíšilová <74699865+hanapp-cz@users.noreply.github.com>
    Co-Authored-By: Matesyi <7796192+Matesyi@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // tady je místo pro náš program
 
-document.querySelector("#vysledek").innerHTML = secti(4, 5);
+//document.querySelector("#vysledek").innerHTML = secti(4, 5);
 
 /**
  * Sečte dvě čísla na vstupu a vrátí výsledek
@@ -8,14 +8,32 @@ document.querySelector("#vysledek").innerHTML = secti(4, 5);
  * @param {int} a
  * @param {int} b
  */
+
 function secti(a, b) {
   let c = a + b;
   return c;
 }
+
+
 
 /**
  * Upozorní uživatele při spuštění.
  */
 function upozorni() {
   alert("Gratulace, právě jsi spustila tuto funkci!");
+}
+console.log("Gratulace, právě jsi spustila tuto funkci!");
+console.log(upozorni);
+
+let nadpis = document.querySelector(".ctverecek");
+nadpis.textContent = "Gratulace";
+//nadpis.style.backgroundColor = "green"
+
+
+function zmen_barvu() {
+  nadpis.style.backgroundColor = "green"
+}
+
+function zobraz_vysledek() {
+  document.querySelector("#vysledek").innerHTML = secti(4, 5);
 }
